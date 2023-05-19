c482b4fc1e21fc83e89735e5741abd1304fd5668
cosmo4 <warner.luke0102@gmail.com>
2023-05-19 12:41:02 -0600

    images added, slidshow somewhat working

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,21 @@
 let newDate = new Date()
 document.getElementById("copyright").innerText = newDate.getFullYear();
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("heroimg");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
